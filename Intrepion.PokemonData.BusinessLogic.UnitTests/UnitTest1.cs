d3d9d1b9a9c4be0fbeea5932726fd1ff91f29d9b
Oliver Forral <intrepion@gmail.com>
2025-02-14 00:24:30 -0700

    dotnet format

---
+++ Intrepion.PokemonData.BusinessLogic.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Intrepion.PokemonData.BusinessLogic.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ Intrepion.PokemonData/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Intrepion.PokemonData.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+{
+}
